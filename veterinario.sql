7c5cc4a694965c87df56d9bbaa52500f4a526a20
LUIZ GUSTAVO CARVALHO DA SILVA COSTA <0000111775618xsp@aluno.educacao.sp.gov.br>
2025-10-24 13:37:23 -0300

    parte 4

--- veterinario.sql
+++ veterinario.sql
@@ -6,5 +6,12 @@ create table login(
 IdLogin int primary key auto_increment,
 loginUser varchar(50) not null,
 senha varchar(10) not null,
-tipo varchar(10)
+tipo varchar(10) not null
+);
+
+create table Cliente(
+idCli int primary key auto_increment,
+nomeCli varchar(50) not null,
+telCli varchar(13) not null,
+emailCli varchar(50) not null
 );
